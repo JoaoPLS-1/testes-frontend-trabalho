7de5428cbb918c615580ca701add6806434f97bb
JoaoPLS-1 <barrancoij@gmail.com>
2026-04-22 18:10:19 -0300

    Exercicio 3 - CheckoutForm - incompleto

--- src/components/CheckoutForm/CheckoutForm.test.tsx
+++ src/components/CheckoutForm/CheckoutForm.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { CheckoutForm } from './CheckoutForm'
+import { wait } from '@testing-library/user-event/dist/cjs/utils/index.js'
 
 /**
  * Exercício 3 — CheckoutForm
@@ -23,6 +24,9 @@ describe('CheckoutForm', () => {
 
     // TODO: verifique que os campos Nome, E-mail e CEP estão presentes
     // Dica: use getByLabelText() buscando pelo texto de cada <label>
+    expect(screen.getByLabelText('Nome')).toBeInTheDocument()
+    expect(screen.getByLabelText('E-mail')).toBeInTheDocument()
+    expect(screen.getByLabelText('CEP')).toBeInTheDocument()
   })
 
   it('exibe erro quando o nome está vazio ao tentar submeter', async () => {
@@ -32,20 +36,30 @@ describe('CheckoutForm', () => {
     await userEvent.click(screen.getByRole('button', { name: /finalizar/i }))
 
     // TODO: verifique que a mensagem "Nome é obrigatório" está na tela
+    expect(screen.getByText('Nome é obrigatório')).toBeInTheDocument()
   })
 
   it('exibe erro quando o e-mail é inválido', async () => {
     // TODO: renderize o CheckoutForm
+    render(<CheckoutForm onSubmit={jest.fn()} />)
     // preencha o campo e-mail com um valor inválido (ex: "nao-é-email")
+    await userEvent.type(screen.getByLabelText('E-mail'), 'nao-email')
+
     // clique em "Finalizar Compra"
+    await userEvent.click(screen.getByRole('button', {name: /Finalizar Compra/i}))
     // e verifique que a mensagem "E-mail inválido" aparece na tela
+    expect(screen.getByText('E-mail inválido')).toBeInTheDocument()
   })
 
   it('exibe erro quando o CEP tem menos de 8 dígitos', async () => {
     // TODO: renderize o CheckoutForm
+    render(<CheckoutForm onSubmit={jest.fn()} />) 
     // preencha o campo CEP com menos de 8 dígitos (ex: "1234")
+    await userEvent.type(screen.getByLabelText('CEP'),'1234')
     // clique em "Finalizar Compra"
+    await userEvent.click(screen.getByRole('button', {name: /Finalizar Compra/i}))
     // e verifique que a mensagem "CEP deve ter 8 dígitos" aparece na tela
+    expect(screen.getByText('CEP deve ter 8 dígitos')).toBeInTheDocument()
   })
 
   it('chama onSubmit com os dados corretos quando o formulário é válido', async () => {
